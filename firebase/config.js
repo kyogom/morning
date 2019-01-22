86d6f6bfc1b6055757568f86112fd04ed02964c4
Kyogo Mochida <kyogo@jp.ibm.com>
2019-01-23 06:30:21 +0900

    react & firebase プロジェクトを追加

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: 'AIzaSyBDM3cEoy-AOJw5PBzN2udI8Eout7prp8Y',
+    authDomain: 'morning-40d4c.firebaseapp.com',
+    databaseURL: 'https://morning-40d4c.firebaseio.com',
+    projectId: 'morning-40d4c',
+    storageBucket: 'morning-40d4c.appspot.com',
+    messagingSenderId: '134509653963'
+};
--- getMessages.sh
+++
@@ -1,5 +0,0 @@
-curl --request GET \
-  --url 'https://slack.com/api/channels.history?token=xoxp-387063691526-476134818017-528168330805-d3ab1a2d79be048fc957ef7b88fcfc83&channel=CDCNR3LUU&count=1000' \
-  --header 'Content-Type: application/x-www-form-urlencoded' \
-  --header 'Postman-Token: 602a9f7f-8195-44b1-93c2-7d74a7556033' \
-  --header 'cache-control: no-cache'
