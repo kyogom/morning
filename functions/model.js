9ae471c9ce09c83d5dec3109ae312b56399556e7
Kyogo Mochida <kyogo@jp.ibm.com>
2019-02-05 08:10:25 +0900

    fix bug #15

--- functions/model.js
+++ functions/model.js
@@ -3,7 +3,7 @@ const service = require('./service').service;
 var tsToDate = (ts) => {
     return new Date(ts * 1000).getFullYear()
         + '-' +
-        new Date(ts * 1000).getMonth() + 1
+        (new Date(ts * 1000).getMonth() + 1)
         + '-' +
         new Date(ts * 1000).getDate();
 };
@@ -11,7 +11,6 @@ var tsToDate = (ts) => {
 var TsToTimePassMinFrom0400 = (ts) => {
     const messageDay = new Date(Number(ts * 1000));
     const messageDay0000ts = new Date(messageDay.getFullYear(), messageDay.getMonth(), messageDay.getDate(), 4).getTime();
-    console.log(new Date(messageDay0000ts));
     return parseInt((ts * 1000 - messageDay0000ts) / 1000 / 60);
 };
 
