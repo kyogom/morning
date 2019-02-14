ea0b428616b2f02cdc74797e3a91e2ff998461a6
Kyogo Mochida <kyogo@jp.ibm.com>
2019-02-14 18:00:30 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 import GetChart from './containers/GetChart';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <GetChart />
-                </header>
+                {/* <header className="App-header"> */}
+                <Header />
+                <GetChart />
+                {/* </header> */}
             </div>
         );
     }
