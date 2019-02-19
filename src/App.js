f79f7937703531821ad9327ee7436725bacd91c9
Kyogo Mochida <kyogo@jp.ibm.com>
2019-02-20 08:17:03 +0900

    add table #28

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import GetChart from './containers/GetChart';
+import DisplayTable from './containers/DisplayTable';
 import Footer from './components/Footer';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { theme } from './theme/theme';
@@ -10,13 +12,16 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <MuiThemeProvider theme={theme}>
-                    <Header />
-                    <main style={{ 'marginTop': 100, 'marginLeft': 60 }}>
-                        <GetChart />
-                    </main>
-                    <Footer />
-                </MuiThemeProvider>
+                <Router>
+                    <MuiThemeProvider theme={theme}>
+                        <Header />
+                        <main style={{ 'marginTop': 100, 'marginLeft': 75 }}>
+                            <Route exact path="/" component={GetChart} />
+                            <Route path="/table" component={DisplayTable} />
+                        </main>
+                        <Footer />
+                    </MuiThemeProvider>
+                </Router>
             </div>
         );
     }
